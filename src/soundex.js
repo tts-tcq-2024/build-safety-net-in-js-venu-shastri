ccbc5b0af44dc6b0ac8480362ce03bee942013f8
Venugopal K <venu.shastri@gmail.com>
2024-06-26 23:38:34 +0530

    Update soundex.js

--- src/soundex.js
+++ src/soundex.js
@@ -1,1 +1,39 @@
+function getSoundexCode(char) {
+    char = char.toUpperCase();
+    const soundexDict = {
+        'B': '1', 'F': '1', 'P': '1', 'V': '1',
+        'C': '2', 'G': '2', 'J': '2', 'K': '2', 'Q': '2', 'S': '2', 'X': '2', 'Z': '2',
+        'D': '3', 'T': '3',
+        'L': '4',
+        'M': '5', 'N': '5',
+        'R': '6'
+    };
+    return soundexDict[char] || '0';
+}
+
+function generateSoundex(name) {
+    if (!name) return '';
+
+    let soundex = [name[0].toUpperCase()];
+    let prevCode = getSoundexCode(name[0]);
+
+    for (let i = 1; i < name.length && soundex.length < 4; i++) {
+        let code = getSoundexCode(name[i]);
+        if (code !== '0' && code !== prevCode) {
+            soundex.push(code);
+        }
+        prevCode = code;
+    }
+
+    while (soundex.length < 4) {
+        soundex.push('0');
+    }
+
+    return soundex.join('');
+}
+
+module.exports = {
+    getSoundexCode,
+    generateSoundex
+};
 
