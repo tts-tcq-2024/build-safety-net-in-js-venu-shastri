810fb6952f51ac8e9bec4518fe08c8e0dd143cb4
Venugopal K <venu.shastri@gmail.com>
2024-06-26 23:37:43 +0530

    Update soundex.test.js

--- test/soundex.test.js
+++ test/soundex.test.js
@@ -1,1 +1,14 @@
+const { expect } = require('chai');
+const { generateSoundex } = require('../soundex');
 
+describe('Soundex Algorithm', () => {
+    it('should handle empty strings', () => {
+        expect(generateSoundex("")).to.equal("");
+    });
+
+    it('should handle single characters', () => {
+        expect(generateSoundex("A")).to.equal("A000");
+    });
+
+    
+});
